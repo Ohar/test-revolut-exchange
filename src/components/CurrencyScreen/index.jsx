692b43ccd1eafa1880fba6c81f59ef9e1389c727
Pavel Lysenko <code@ohar.name>
2018-12-16 21:01:22 +0300

    CurrencySelector: separate “CurrencyScreen”

---
+++ src/components/CurrencyScreen/index.jsx
@@ -0,0 +1,81 @@
+import currencyList from '@/consts/currencyList'
+import currencySelectorStates from '@/consts/currencySelectorStates'
+import actionCurrencyFromSet from '@/store/currency/actions/currency-from-set'
+import actionCurrencyToSet from '@/store/currency/actions/currency-to-set'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import './style.less'
+import { bindActionCreators } from 'redux'
+
+class CurrencyScreen extends Component {
+  constructor () {
+    super()
+
+    this.changeCurrency = this.changeCurrency.bind(this)
+    this.toNextCurrency = this.toNextCurrency.bind(this)
+    this.toPrevCurrency = this.toPrevCurrency.bind(this)
+  }
+
+  changeCurrency (currencyIndex) {
+    const {type, currencyFromSet, currencyToSet}  = this.props
+    const changer = type === currencySelectorStates.from
+                    ? currencyFromSet
+                    : currencyToSet
+
+    changer(currencyList[currencyIndex])
+  }
+
+  toNextCurrency () {
+    const {currency}  = this.props
+    const currentIndex = currencyList.indexOf(currency)
+    const nextIndex = currentIndex === currencyList.length - 1
+                      ? 0
+                      : currentIndex + 1
+
+    this.changeCurrency(nextIndex)
+  }
+
+  toPrevCurrency () {
+    const {currency}  = this.props
+    const currentIndex = currencyList.indexOf(currency)
+    const prevIndex = currentIndex === 0
+                      ? currencyList.length - 1
+                      : currentIndex - 1
+
+    this.changeCurrency(prevIndex)
+  }
+
+  render () {
+    const {currency, type}  = this.props
+    const current = this.props[type] // “from” or “to”
+
+    return (
+      <li className='CurrencyScreen'>
+        <button onClick={this.toPrevCurrency}>←</button>
+        {currency}
+        {currency === current ? '+' : '-'}
+        <button onClick={this.toNextCurrency}>→</button>
+      </li>
+    )
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return bindActionCreators(
+    {
+      currencyFromSet: actionCurrencyFromSet,
+      currencyToSet  : actionCurrencyToSet,
+    },
+    dispatch,
+  )
+}
+
+function mapStateToProps (state, ownProps) {
+  return {
+    ...ownProps,
+    [currencySelectorStates.from]: state.currencyState.from,
+    [currencySelectorStates.to]  : state.currencyState.to,
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CurrencyScreen)
--- src/components/CurrencySelector/index.jsx
+++ src/components/CurrencySelector/index.jsx
@@ -1,58 +1,22 @@
 import currencyList from '@/consts/currencyList'
-import currencySelectorStates from '@/consts/currencySelectorStates'
-import actionCurrencyFromSet from '@/store/currency/actions/currency-from-set'
-import actionCurrencyToSet from '@/store/currency/actions/currency-to-set'
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import CurrencyScreen from '@/components/CurrencyScreen'
+import React from 'react'
 import './style.less'
-import { bindActionCreators } from 'redux'
 
-class CurrencySelector extends Component {
-  render () {
-    const {currencyFromSet, currencyToSet, type}  = this.props
-    const current = this.props[type] // “from” or “to”
-    const changer = type === currencySelectorStates.from
-                    ? currencyFromSet
-                    : currencyToSet
-
-    // TODO: separate <LI> as component
-    return (
-      <ul className='CurrencySelector'>
-        {
-          currencyList.map(
-            currency => (
-              <li
-                className='CurrencySelector_item'
-                key={currency}
-                onClick={() => changer(currency)}
-              >
-                {currency}
-                {currency === current ? '+' : '-'}
-              </li>
-            ),
-          )
-        }
-      </ul>
-    )
-  }
-}
-
-function mapDispatchToProps (dispatch) {
-  return bindActionCreators(
-    {
-      currencyFromSet: actionCurrencyFromSet,
-      currencyToSet  : actionCurrencyToSet,
-    },
-    dispatch,
+export default function CurrencySelector ({type}) {
+  return (
+    <ul className='CurrencySelector'>
+      {
+        currencyList.map(
+          currency => (
+            <CurrencyScreen
+              key={currency}
+              currency={currency}
+              type={type}
+            />
+          ),
+        )
+      }
+    </ul>
   )
 }
-
-function mapStateToProps (state, ownProps) {
-  return {
-    ...ownProps,
-    [currencySelectorStates.from]: state.currencyState.from,
-    [currencySelectorStates.to]  : state.currencyState.to,
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(CurrencySelector)

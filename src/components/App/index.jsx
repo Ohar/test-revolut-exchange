de9bdf698ce5f34d4455411d7d4e317967566747
Pavel Lysenko <code@ohar.name>
2018-12-16 19:04:18 +0300

    Add StatusBar

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,12 +1,14 @@
 import Exchanger from '@/components/Exchanger/index'
 import Controls from '@/components/Controls/index'
 import Keyboard from '@/components/Keyboard/index'
+import StatusBar from '@/components/StatusBar'
 import React from 'react'
 import './style.less'
 
 export default function App () {
   return (
     <main className='App'>
+      <StatusBar/>
       <Controls/>
       <Exchanger/>
       <Keyboard/>

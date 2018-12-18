5a0e90570992a3f7f9ff23a3a8be7009cd79b6c0
Pavel Lysenko <code@ohar.name>
2018-12-19 00:12:41 +0300

    MONEY_FROM_SET: count “moneyTo”

--- src/store/currency/reducer.js
+++ src/store/currency/reducer.js
@@ -1,5 +1,6 @@
 import * as actionTypes from '@/store/currency/action-types'
 import DEFAULT_STATE from '@/store/currency/default_state'
+import countMoneyTo from '@/utils/countMoneyTo'
 
 export default function dataReducer (state = DEFAULT_STATE, action) {
   switch (action.type) {
@@ -26,8 +27,13 @@ export default function dataReducer (state = DEFAULT_STATE, action) {
     }
 
     case actionTypes.MONEY_FROM_SET: {
+      const rateFrom = state.rate[state.currencyFrom]
+      const rateTo   = state.rate[state.currencyTo]
+      const moneyTo  = countMoneyTo(action.moneyFrom, rateFrom, rateTo)
+
       return {
         ...state,
+        moneyTo,
         moneyFrom: action.moneyFrom,
       }
     }

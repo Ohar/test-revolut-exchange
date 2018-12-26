394a58f644e8365dea0510e34de1c46fddf52260
Pavel Lysenko <code@ohar.name>
2018-12-26 03:26:02 +0300

    CurrencyScreen: remove exceed methods

--- src/components/CurrencyScreen/index.jsx
+++ src/components/CurrencyScreen/index.jsx
@@ -1,58 +1,13 @@
 import AccountInfo from '@/components/AccountInfo'
 import MoneyInput from '@/components/MoneyInput'
 import RateInfo from '@/components/RateInfo'
-import currencyList from '@/consts/currencyList'
 import currencySelectorStates from '@/consts/currencySelectorStates'
-import actionCurrencyFromSet from '@/store/actions/currency-from-set'
-import actionCurrencyToSet from '@/store/actions/currency-to-set'
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
+import React from 'react'
 import './style.less'
 
-class CurrencyScreen extends Component {
-  constructor () {
-    super()
-
-    this.changeCurrency = this.changeCurrency.bind(this)
-    this.toNextCurrency = this.toNextCurrency.bind(this)
-    this.toPrevCurrency = this.toPrevCurrency.bind(this)
-  }
-
-  changeCurrency (currencyIndex) {
-    const {type, currencyFromSet, currencyToSet} = this.props
-    const changer                                = type === currencySelectorStates.from
-                                                   ? currencyFromSet
-                                                   : currencyToSet
-
-    changer(currencyList[currencyIndex])
-  }
-
-  toNextCurrency () {
-    const {currency}   = this.props
-    const currentIndex = currencyList.indexOf(currency)
-    const nextIndex    = currentIndex === currencyList.length - 1
-                         ? 0
-                         : currentIndex + 1
-
-    this.changeCurrency(nextIndex)
-  }
-
-  toPrevCurrency () {
-    const {currency}   = this.props
-    const currentIndex = currencyList.indexOf(currency)
-    const prevIndex    = currentIndex === 0
-                         ? currencyList.length - 1
-                         : currentIndex - 1
-
-    this.changeCurrency(prevIndex)
-  }
-
-  render () {
-    const {currency, type} = this.props
-
-    return (
-      <li className='CurrencyScreen'>
+export default function CurrencyScreen ({currency, type}) {
+  return (
+    <li className='CurrencyScreen'>
         <span className='CurrencyScreen_row'>
           <span className='CurrencyScreen_currency'>
             {currency}
@@ -61,36 +16,15 @@ class CurrencyScreen extends Component {
           <MoneyInput type={type}/>
         </span>
 
-        <span className='CurrencyScreen_row'>
+      <span className='CurrencyScreen_row'>
           <AccountInfo currency={currency}/>
 
-          {
-            type === currencySelectorStates.to
-            ? <RateInfo/>
-            : null
-          }
+        {
+          type === currencySelectorStates.to
+          ? <RateInfo/>
+          : null
+        }
         </span>
-      </li>
-    )
-  }
-}
-
-function mapDispatchToProps (dispatch) {
-  return bindActionCreators(
-    {
-      currencyFromSet: actionCurrencyFromSet,
-      currencyToSet  : actionCurrencyToSet,
-    },
-    dispatch,
+    </li>
   )
 }
-
-function mapStateToProps (state, ownProps) {
-  return {
-    ...ownProps,
-    [currencySelectorStates.from]: state.currencyState.currencyFrom,
-    [currencySelectorStates.to]  : state.currencyState.currencyTo,
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(CurrencyScreen)

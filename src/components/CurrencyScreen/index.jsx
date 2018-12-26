05fa40c77faec8e92c4d5ca6687ffeb196786b89
Pavel Lysenko <code@ohar.name>
2018-12-26 03:26:40 +0300

    CurrencyScreen: fix code style

--- src/components/CurrencyScreen/index.jsx
+++ src/components/CurrencyScreen/index.jsx
@@ -8,16 +8,16 @@ import './style.less'
 export default function CurrencyScreen ({currency, type}) {
   return (
     <li className='CurrencyScreen'>
-        <span className='CurrencyScreen_row'>
-          <span className='CurrencyScreen_currency'>
-            {currency}
-          </span>
-
-          <MoneyInput type={type}/>
+      <span className='CurrencyScreen_row'>
+        <span className='CurrencyScreen_currency'>
+          {currency}
         </span>
 
+        <MoneyInput type={type}/>
+      </span>
+
       <span className='CurrencyScreen_row'>
-          <AccountInfo currency={currency}/>
+        <AccountInfo currency={currency}/>
 
         {
           type === currencySelectorStates.to
